300b3ad4aae1bdd2c8a1d1164ab47aaac40824b6
tiarahasnabila <tiarahasnabila@gmail.com>
2025-08-01 16:16:44 +0700

    ubah web.php

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,15 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
+
+// ROUTE LOGOUT MANUAL - TAMBAHAN BARU
+Route::get('/keluar', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect()->route('login')->with('status', 'Berhasil logout!');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -34,7 +43,38 @@ Route::middleware('auth')->group(function () {
     // Route::get('/freelancer', action: [UserController::class, 'index'])->name('freelancer.index');
     // Route::get('/admin', action: [UserController::class, 'index'])->name('admin.index');
 
+    //BARU
+    Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route yang sudah ada
+    // Route::get('/client', [UserController::class, 'index'])->name('client.index');
+
+    // Tambahkan route baru ini
+    Route::get('/client-dashboard', [UserController::class, 'index'])->name('client.dashboard');
+
+    Route::get('ini-untuk-client',function(){
+    return "Ini client";
+})->name('ini.client')->middleware('role:client');
+
+Route::get('ini-untuk-freelancer',function(){
+    return "Ini freelancer";
+})->name('ini.freelancer')->middleware('role:freelancer');
+});
+
+
+    // Tambahkan route ini untuk /dashboard
+    Route::get('/dashboardtu', [UserController::class, 'index'])->name('user.dashboard');
+
+    Route::get('/', function () {
+    return redirect()->route('login');
+    })->name('landing');
+
+
 
 });
-// 
+
+//
 require __DIR__.'/auth.php';
